0d7ddec3d8effdd291c44d7111e0ee6c3198b2c1
Kevin Nimi <k.nimi73@gmail.com>
2025-02-05 22:08:52 -0500

    add Product route and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import express from "express";
+import express, { json, urlencoded, Request } from "express";
+import productsRoutes from "./routes/products/index.js";
 
 const app = express();
 const port = 3000;
 
+app.use(urlencoded({ extended: false }));
+// app.use(
+//   json({
+//     verify: (req: Request, res, buf) => {
+//       req.rawBody = buf;
+//     },
+//   })
+// );
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//End Point
+app.use("/products", productsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/routes/products/productsController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+
+export async function listProducts(req: Request, res: Response) {
+  try {
+    res.json("Hello products!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
+
+export async function getProductById(req: Request, res: Response) {
+  try {
+    res.json("Hello products!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
+
+export async function createProduct(req: Request, res: Response) {
+  try {
+    res.json("Hello products!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
+
+export async function updateProduct(req: Request, res: Response) {
+  try {
+    res.json("Hello products!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
+
+export async function deleteProduct(req: Request, res: Response) {
+  try {
+    res.json("Hello products!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
